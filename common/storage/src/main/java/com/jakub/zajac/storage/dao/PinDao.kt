f458ab35afe5856468908626cec18457a5da5d32
Jakub <jakub.zajac44@gmail.com>
2024-09-17 19:46:16 +0200

    Add update pin method Change pinCode to String

--- common/storage/src/main/java/com/jakub/zajac/storage/dao/PinDao.kt
+++ common/storage/src/main/java/com/jakub/zajac/storage/dao/PinDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.jakub.zajac.storage.model.PinEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -17,6 +18,9 @@ interface PinDao {
     @Delete
     suspend fun deletePin(pinEntity: PinEntity)
 
+    @Update
+    suspend fun updatePin(pinEntity: PinEntity)
+
     @Query("Select * from PinEntity")
-    suspend fun getAllPin(): Flow<List<PinEntity>>
+    fun getAllPin(): Flow<List<PinEntity>>
 }
